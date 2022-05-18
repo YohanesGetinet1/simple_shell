dd41387ccd90f2e850273fd996f3ca07834381a7
Yohanes Getinet <74892375+YohanesGetinet1@users.noreply.github.com>
2022-05-18 16:43:12 +0530

    Update main.h

--- main.h
+++ main.h
@@ -1,5 +1,5 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
 
 /**
@@ -76,5 +76,34 @@ char *_strcat(char *dest, char *src);
 int _strlen(char *s);
 
 void piderror(pid_t pid, char *buff, char **commands);
+
+typedef struct built_in
+{
+	char *str;
+	struct built_in *next;
+} built_in_t;
+
+void direc_fat(char *str, char **temp);
+void obt_dir(void);
+void handle_sigint(int sig);
+int _strcom (char *str_1, char *str_2);
+void _cd(char *arg);
+extern char **environ;
+void print_env(void);
+void error(char *str, int cont, int flag);
+int _strlen(char *s);
+void rev_string(char *s);
+char *_itoa(int n);
+unsigned int get_pid(void);
+unsigned int get_ppid(void);
+char *stringdup(const char *str);
+int toksize(char *str);
+char **tokenize(char *str);
+int exec(pid_t c_pid, char **array, int cont);
+int str_comp(char **array, int size);
+char *_getenv(const char *name);
+int _which(char *file);
+char *str_concat(char *s1, char *s2, int flag);
+int path(char **array, int cont);
 
 #endif
